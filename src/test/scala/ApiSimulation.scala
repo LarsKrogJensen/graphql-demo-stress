d6aec44dec8e1ed129e9c6f0d9f1fa79fe4ab29a
Lars Krog-Jensen <lars.krogjensen@gmail.com>
2017-03-25 10:19:38 +0100

    trying to do auth

--- src/test/scala/ApiSimulation.scala
+++ src/test/scala/ApiSimulation.scala
@@ -1,6 +1,6 @@
 import io.gatling.core.Predef._
 import io.gatling.core.body.StringBody
-import io.gatling.core.structure.{PopulationBuilder, ScenarioBuilder}
+import io.gatling.core.structure.{ChainBuilder, PopulationBuilder, ScenarioBuilder}
 import io.gatling.http.Predef._
 import io.gatling.http.request.builder.HttpRequestBuilder
 import scenarios.ListingScenarios
@@ -11,16 +11,17 @@ class ApiSimulation extends Simulation {
 
   val API_URL: String = System.getProperty("api_url", "http://localhost:8080")
   val DURATION: Int = 60
-  val LOAD_FACTOR: Double = 200
+  val LOAD_FACTOR: Double = 4
   var RAMP_UP_TIME: Int = 1
   val RAMP_USER_PER_SEC = 0.1
 
 
   private val httpConf =
     http.baseURL(API_URL)
+//      .proxy(Proxy("localhost", 8888))
       .acceptHeader("application/json")
-      .authorizationHeader(fetchToken)
-      .acceptEncodingHeader("gzip, deflate")
+//      .authorizationHeader(fetchToken)
+//      .acceptEncodingHeader("gzip, deflate")
       .acceptLanguageHeader("en-US,en;q=0.5")
       .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
@@ -31,16 +32,29 @@ class ApiSimulation extends Simulation {
       .protocols(httpConf)
 
   private def fetchToken: String = {
-//
-//    exec()
-//    http.baseURL(API_URL + "/authenticate").get
-//      .post()
-
+    //session
+    scn.
     return "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJPaWtLQWZxSFkyeHBaVzUwU1VSQ2MybDRpMk55WldGMGFXOXVWR2x0WlZ3eU1ERTNMVEF6TFRJMFZERTRPalUzT2pRM0xqYzFNeXN3TVRvd01Qcz0uYWI2NGViNTZhZDA0NGY3ZTFkNTYzMzZiZGNkNGE3MzRlNTBkODBiNCIsImlhdCI6MTQ5MDM3ODI2N30=.cak2s59LALdX28o9EUFdnvuh6sWXFThMMAvsfvRMGFY="
   }
 
+
+      val scn = scenario("Authenticate")
+        .exec(
+          http("Authenticate")
+            .post("/authenticate")
+            .body(StringBody("""{"client_id": "six", "client_secret" : "sixsixsix"}"""))
+            .asJSON
+            .check(status.is(200))
+            .check(jsonPath("$..access_token").find.saveAs("authToken"))
+        )
+
+  before(
+    scn.inject(atOnceUsers(1)).protocols(httpConf)
+  )
+
   setUp(
-    withDefaults(ListingScenarios.listingQuery)
+    ListingScenarios.listingQuery.inject(atOnceUsers(4)).protocols(httpConf)
+    //withDefaults(ListingScenarios.listingQuery)
   )
 
 }
--- src/test/scala/scenarios/ListingScenarios.scala
+++ src/test/scala/scenarios/ListingScenarios.scala
@@ -1,6 +1,7 @@
 package scenarios
 
 import io.gatling.core.Predef._
+import io.gatling.core.body.StringBody
 import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
 
@@ -13,13 +14,25 @@ object ListingScenarios {
       | "query": "{ listing(id: \"${tsid}\") { id }}"
       |}
     """.stripMargin
+//  val sessionHeaders = Map("Authorization" -> "Bearer ${authToken}",
+//                             "Content-Type" -> "application/json")
+
 
   val listingQuery: ScenarioBuilder = scenario("Listings")
+//    .exec(
+//      http("Authenticate")
+//        .post("/authenticate")
+//        .body(StringBody("""{"client_id": "six", "client_secret" : "sixsixsix"}"""))
+//        .asJSON
+//        .check(status.is(200))
+//        .check(jsonPath("$..access_token").find.saveAs("token"))
+//    )
     .feed(equityTsids)
     .exec(
       http("listing-query")
         .post("/graphql")
         .body(StringBody(query))
+        .header("authorization", "Bearer ${authToken}")
         .check(status.is(200))
         .check(regex("listing"))
     )
